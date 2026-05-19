b9e084c51f490eeb3ce879966e870f6b72aee99e
Naduka Yomal <naduka898@gmail.com>
2026-05-20 00:47:13 +0530

    done tuple

---
+++ data-structure/tuple.py
@@ -0,0 +1,11 @@
+details = ('Kamal', '20000', 'Pallekele Kundasale', 24)
+name = details[0]
+print(details)
+print(name)
+print(details.count(name))
+
+name, dob, address, age = details
+print(name)
+print(dob)
+print(address)
+print(age)
