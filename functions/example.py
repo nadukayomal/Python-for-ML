380829685cc992c334e79deb23247dbf905d327d
Naduka Yomal <naduka898@gmail.com>
2026-06-01 00:07:53 +0530

    possitional argument

--- functions/example.py
+++ functions/example.py
@@ -9,3 +9,23 @@ def assign_marks(mark):
 
 get_mark = assign_marks(40)
 print(get_mark)
+
+
+""" 
+    Positional parameter 
+        - We need to pass the values as we defined order
+        - We can't define argument after positional parameter
+"""
+
+def brand_price(brand, price):
+    print(brand)
+    if price >= 1000:
+        print("gold")
+    elif price >= 500:
+        print("silver")
+    else:
+        print("bronz")
+
+
+brand_price("apple", 100)
+# brand_price("apple", '100') # this occur an error
