603a74fc1e4ae950b3f0d7ae996e2a51925a3a3f
Naduka Yomal <naduka898@gmail.com>
2026-06-01 00:16:39 +0530

    Named arguments

--- functions/example.py
+++ functions/example.py
@@ -29,3 +29,21 @@ def brand_price(brand, price):
 
 brand_price("apple", 100)
 # brand_price("apple", '100') # this occur an error
+
+"""
+    Name argument
+        - if we add name argument to the function , all other argument should be named argument and put the right side 
+"""
+
+def brand_price_1(brand, price):
+    print(brand)
+    if price >= 1000:
+        print("gold")
+    elif price >= 500:
+        print("silver")
+    else:
+        print("bronz")
+
+brand_price_1(brand='Toyota', price=100000)
+# brand_price_1(brand='Toyota', 100000) # occur some error
+brand_price_1('Toyota', price=100000)
