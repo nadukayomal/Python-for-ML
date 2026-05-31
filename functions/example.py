4e74f96fd99574a98ff564cd7662905e43e8d96c
Naduka Yomal <naduka898@gmail.com>
2026-06-01 00:37:24 +0530

    done packed & Kwarg

--- functions/example.py
+++ functions/example.py
@@ -47,3 +47,27 @@ def brand_price_1(brand, price):
 brand_price_1(brand='Toyota', price=100000)
 # brand_price_1(brand='Toyota', 100000) # occur some error
 brand_price_1('Toyota', price=100000)
+
+
+"""
+    Packed args & Kwargs
+        - We passed the too many parameter to the same argument
+        - We can use packed and kwargs in same function but.
+            always should keep kwargs after the packed
+"""
+# this is packed
+def get_grade(*marks):
+    total = 0
+    for i in marks:
+        total += 1
+    print(total)
+
+get_grade(10, 20, 30)
+
+# this is kwarg
+def my_form(**params):
+    print(params)
+
+my_form(name="John", city="New York", age=23)
+
+
